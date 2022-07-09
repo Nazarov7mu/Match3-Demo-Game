f230f170c95afecbc2b90aa9a742113ad5d8b995
Javokhir Nazarov <nazarov7mu@gmail.com>
2022-07-09 15:38:02 +0900

    Initialize the Grid and populate it with Containers and Tiles

--- Assets/RG/Match3/Scripts/GameData.cs
+++ Assets/RG/Match3/Scripts/GameData.cs
@@ -2,7 +2,18 @@ using UnityEngine;
 
 namespace RG.Match3.Scripts {
     public class GameData : MonoBehaviour {
+        [Range(4, 10)] [SerializeField] private int _numOfRows = 10;
+        [Range(4, 10)] [SerializeField] private int _numOfColumns = 10;
+    
+        public int NumOfRows => _numOfRows; 
+        public int NumOfColumns => _numOfColumns; 
+    
+        public const float StartPosX = -4.5f;
+        public const float StartPosY = 4.5f;
+        public const float HopSize = 1f;
         
+        public Container[][] Grid { get; set; } // Grid of Containers
+
     }
 
     public enum TileShape {
